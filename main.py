b83ab389098f67a309bcdd8f372f22e893d76f71
KangWon LEE <kangwon.lee.edu@gmail.com>
2020-11-02 09:19:51 +0000

    intermediate steps

--- main.py
+++ main.py
@@ -19,21 +19,29 @@ def heun_step(f:SlopeFunction, x0:State, t0:float, t1:float) -> State:
     t0 : this step time
     t1 : next step time
     """
+    print(f"x at the current step = {x0}")
+    print(f"t at the current step t0 = {t0}")
+    print(f"t at the next step t1 = {t1}")
 
     # time step
     delta_t = t1 - t0
+    print(f"delta_t = {delta_t}")
 
     # slope
     s1 = f(t0, x0)
+    print(f"slope at t0 = {s1}")
 
     # next step by Euler
     x1_euler = x0 + s1 * delta_t
+    print(f"estimated x at the next step by Euler's method = {x1_euler}")
 
     # slope at next step
     s2 = f(t1, x1_euler)
+    print(f"estimated slope at t1 using x1 from Euler's method = {s1}")
 
     # average of two slopes
     s_average = (s1 + s2) * 0.5
+    print(f"average of two slopes (({s1}) + ({s2})) / 2 = {s_average}")
 
     # next step by Heun's method
     x1 = x0 + s_average * delta_t
